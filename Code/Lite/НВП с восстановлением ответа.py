b9f17ef3bd5bf86a0bc92bcd73c66592b5fa6ed7
Shipov_maxim <Shipov.max@icloud.com>
2025-10-02 21:04:55 +0300

    Убрал комменты

--- Code/Lite/НВП с восстановлением ответа.py
+++ Code/Lite/НВП с восстановлением ответа.py
@@ -1,6 +1,5 @@
 def main():
 
-    # Функция по нахождению НВП с восстановлением ответа
     def longest_increasing_subsequence(arr):
         n = len(arr)
         matrix = [1] * n
@@ -11,23 +10,18 @@ def main():
                     matrix[i] = matrix[j] + 1
                     prev[i] = j
 
-        # Находим индекс последнего элемента НВП
         last_index = matrix.index(max(matrix))
 
-        # Восстанавливаем НВП
         lis = []
         while last_index != -1:
             lis.append(arr[last_index])
             last_index = prev[last_index]
 
-        # Разворачиваем, так как построили с конца
         return lis[::-1]
 
-    # Чтение входных данных
     n = int(input())
     sequence = list(map(int, input().split()))
 
-    # Находим НВП и вывод результата
     print(" ".join(map(str, longest_increasing_subsequence(sequence))))
 
 
